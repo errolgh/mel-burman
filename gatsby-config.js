9b8b411ba580b2f1406570ed21d77edc27877faf
Errol Watson <errolwatson@Errols-MacBook-Air.local>
2021-05-24 22:45:46 -0400

    added config for source files

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,27 @@ module.exports = {
     description: `A Baltimore-based artist`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
