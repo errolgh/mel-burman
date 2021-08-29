5b8d43362bccd38fcddec989c0e8c0067f97040f
Errol Watson <errolwatson@Errols-MacBook-Air.local>
2021-08-28 23:37:01 -0400

    nav dev complete, tweaks

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -47,6 +47,7 @@ const DropdownItem = styled.li`
     background: #fafafa;
     cursor: pointer;
     padding: 5px;
+
     &:hover {
         background: #E9E9E9;
     }
@@ -83,38 +84,3 @@ function Dropdown () {
 }
 
 export default Dropdown
-
-
-
-/*
-.dropdown-menu {
-    width: 200px;
-    position: absolute;
-    top: 80px;
-    list-style: none;
-    text-align: start;
-}
-
-.dropdown-menu li {
-    background: #1888ff;
-    cursor: pointer;
-}
-
-.dropdown-menu li:hover {
-    background: #5cabff;
-}
-
-.dropdown-menu.clicked {
-    display: none;
-}
-
-.dropdown-link {
-    display: block;
-    width: 100%;
-    height: 100%;
-    text-decoration: none;
-    color: #fff;
-    padding: 16px;
-}
-
-*/
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,10 +8,10 @@ import Dropdown from './dropdown'
 const NavBar = styled.nav`
     display: flex;
     justify-content: space-between;
+    align-items: center;
     max-width: 1200px;
     margin: 0 auto;
     width: 100%;
-    align-items: center;
     height: 10vh;
     padding: 30px;
 
@@ -27,19 +27,19 @@ const Logo = styled.div`
 
 const LinksList = styled.ul`
     display: flex;
+    position: absolute;
+    top: 10vh;
+    left: -100%;
     flex-direction: column;
     justify-content: start;
     width: 100%;
     height: 90vh;
+    margin: 0;
+    padding: 0;
+    background: #fafafa;
     font-size: 16px;
     font-weight: lighter;
     color: #595959;
-    background: #fafafa;
-    margin: 0;
-    padding: 0;
-    position: absolute;
-    top: 10vh;
-    left: -100%;
     opacity: 0;
     transition: all 0.6s ease;
     
@@ -55,7 +55,7 @@ const LinksList = styled.ul`
     &.active {
         left: 0;
         opacity: 1;
-        z-index: 1;
+        /* z-index: 1; */
     }
     `;
 
@@ -77,14 +77,12 @@ const ListItem = styled.li`
     }
 `;
 
-const DropdownLinkContainer = styled.div`
-
-`;
+const DropdownLinkContainer = styled.div``;
 
 const DropdownListContainer = styled.div`
     position: relative;
-    width: 100%;
-    
+    /* width: 100%; */
+    /* z-index: 1000; */
 
     @media(min-width: 768px) {
         /* padding-top: 5px; */
@@ -106,8 +104,7 @@ const HamburgerIcon = styled.div`
 
 const Nav = () => {
     const [ click, setClick ] = useState(false)
-    // const [ dropdown, setDropdown ] = useState(false) /////// testing state
-    const [ dropdown, setDropdown ] = useState(true)
+    const [ dropdown, setDropdown ] = useState(false)
 
     const handleClick = () => setClick(!click)  // toggle hamburger menu icon
     const closeMobileMenu = () => setClick(false)
@@ -168,159 +165,3 @@ const Nav = () => {
 }
 
 export default Nav
-
-
-/*
-
-.navbar {
-    background: linear-gradient(90deg, rgb(28, 27, 27) 0%, rgb(26, 23, 23) 100%);
-    height: 80px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 1.2rem;
-  }
-  
-  .navbar-logo {
-    color: #fff;
-    justify-self: start;
-    margin-left: 20px;
-    cursor: pointer;
-    text-decoration: none;
-    font-size: 2rem;
-  }
-  
-  .fa-firstdraft {
-    margin-left: 0.5rem;
-    font-size: 1.6rem;
-  }
-  
-  .nav-menu {
-    display: grid;
-    grid-template-columns: repeat(5, auto);
-    grid-gap: 10px;
-    list-style: none;
-    text-align: center;
-    width: 70vw;
-    justify-content: end;
-    margin-right: 2rem;
-  }
-  
-  .nav-item {
-    display: flex;
-    align-items: center;
-    height: 80px;
-  }
-  
-  .nav-links {
-    color: white;
-    text-decoration: none;
-    padding: 0.5rem 1rem;
-  }
-  
-  .nav-links:hover {
-    background-color: #1888ff;
-    border-radius: 4px;
-    transition: all 0.2s ease-out;
-  }
-  
-  .fa-bars {
-    color: #fff;
-  }
-  
-  .nav-links-mobile {
-    display: none;
-  }
-  
-  .menu-icon {
-    display: none;
-  }
-  
-  @media screen and (max-width: 960px) {
-    .fa-caret-down {
-      display: none;
-    }
-    .NavbarItems {
-      position: relative;
-    }
-  
-    .nav-menu {
-      display: flex;
-      flex-direction: column;
-      justify-content: start;
-      width: 100%;
-      height: 90vh;
-      position: absolute;
-      top: 80px;
-      left: -100%;
-      opacity: 1;
-      transition: all 0.5s ease;
-    }
-  
-    .nav-menu.active {
-      background: #242222;
-      left: 0;
-      opacity: 1;
-      transition: all 0.5s ease;
-      z-index: 1;
-    }
-  
-    .nav-links {
-      text-align: center;
-      padding: 2rem;
-      width: 100%;
-      display: table;
-    }
-  
-    .nav-links:hover {
-      background-color: #1888ff;
-      border-radius: 0;
-    }
-  
-    .navbar-logo {
-      position: absolute;
-      top: 0;
-      left: 0;
-      transform: translate(25%, 50%);
-    }
-  
-    .menu-icon {
-      display: block;
-      position: absolute;
-      top: 0;
-      right: 0;
-      transform: translate(-100%, 60%);
-      font-size: 1.8rem;
-      cursor: pointer;
-    }
-  
-    .fa-times {
-      color: #fff;
-      font-size: 2rem;
-    }
-  
-    .nav-links-mobile {
-      display: block;
-      text-align: center;
-      padding: 1.5rem;
-      margin: 2rem auto;
-      border-radius: 4px;
-      width: 80%;
-      background: #1888ff;
-      text-decoration: none;
-      color: #fff;
-      font-size: 1.5rem;
-    }
-  
-    .nav-links-mobile:hover {
-      background: #fff;
-      color: #1888ff;
-      transition: 250ms;
-    }
-  
-    button {
-      display: none;
-    }
-}
-
-*/
